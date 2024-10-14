55587b5d0737c5074b2f3475ce28c87fdf217f1c
kzherdinnn <herdinkz2003@gmail.com>
2024-10-14 18:05:45 +0700

    Initial commit

--- app/src/main/java/lat/pam/droidcafe/OrderActivity.kt
+++ app/src/main/java/lat/pam/droidcafe/OrderActivity.kt
@@ -2,24 +2,43 @@ package lat.pam.droidcafe
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.RadioButton
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class OrderActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order)
+
+        // Inisialisasi Spinner untuk pemilihan kota
+        val citySpinner: Spinner = findViewById(R.id.city_spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.city_array,  // Gunakan array city yang didefinisikan di strings.xml
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            citySpinner.adapter = adapter
         }
+    }
 
     fun onRadioButtonClicked(view: View) {
         val checked = (view as RadioButton).isChecked
+        when (view.id) {
+            R.id.sameday -> if (checked)
                 displayToast(getString(R.string.same_day_messenger_service))
+            R.id.nextday -> if (checked)
                 displayToast(getString(R.string.next_day_ground_delivery))
+            R.id.pickup -> if (checked)
                 displayToast(getString(R.string.pick_up))
         }
     }
 
     private fun displayToast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
     }
 }
